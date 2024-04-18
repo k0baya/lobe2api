fd2b9e3a0b6a63f9c54a7c4d3fb63297a6b2acda
k0baya <132335755+k0baya@users.noreply.github.com>
2024-04-18 20:24:14 +0800

    Streamline Functions

--- server.js
+++ server.js
@@ -38,34 +38,6 @@ function GenerateCompletionId(prefix = "cmpl-") {
   return prefix;
 }
 
-async function* chunksToLines(chunksAsync) {
-  let previous = "";
-  for await (const chunk of chunksAsync) {
-    const bufferChunk = Buffer.isBuffer(chunk) ? chunk : Buffer.from(chunk);
-    previous += bufferChunk;
-    let eolIndex;
-    while ((eolIndex = previous.indexOf("\n")) >= 0) {
-      // line includes the EOL
-      const line = previous.slice(0, eolIndex + 1).trimEnd();
-      if (line === "data: [DONE]") break;
-      if (line.startsWith("data: ")) yield line;
-      previous = previous.slice(eolIndex + 1);
-    }
-  }
-}
-
-async function* linesToMessages(linesAsync) {
-  for await (const line of linesAsync) {
-    const message = line.substring("data :".length);
-
-    yield message;
-  }
-}
-
-async function* StreamCompletion(data) {
-  yield* linesToMessages(chunksToLines(data));
-}
-
 // Function to create JWT
 function createJWT() {
   const now = Math.floor(Date.now() / 1000);
